afa9d9893b994d4450ad2c348cdcf3a50c4f18f1
biancapower <bianca@biancapower.com>
2020-06-15 08:14:34 +1000

    separate out parts of uri

--- jokes.rb
+++ jokes.rb
@@ -17,6 +17,12 @@ blacklist = {
   sexist: true,
 }
 
-#todo: dynamically generate this link based on true / false of above values
-response = HTTParty.get('https://sv443.net/jokeapi/v2/joke/Programming,Miscellaneous,Dark?blacklistFlags=nsfw,religious,political,racist,sexist')
+base_uri = 'https://sv443.net/jokeapi/v2/joke/'
+category_string = 'Programming,Miscellaneous,Dark'
+blacklist_string = 'nsfw,religious,political,racist,sexist'
+uri = base_uri + category_string + '?blacklistFlags=' + blacklist_string
 
+#todo: dynamically generate this link based on true / false of above values
+#response = HTTParty.get('https://sv443.net/jokeapi/v2/joke/Programming,Miscellaneous,Dark?blacklistFlags=nsfw,religious,political,racist,sexist')
+response = HTTParty.get(uri)
+p response.parsed_response["category"]
