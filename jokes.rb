b32a522e235732452f28ab86ba680851c194100f
biancapower <bianca@biancapower.com>
2020-06-15 08:30:55 +1000

    build up category string based on hash

--- jokes.rb
+++ jokes.rb
@@ -18,11 +18,24 @@ blacklist = {
 }
 
 base_uri = 'https://sv443.net/jokeapi/v2/joke/'
-category_string = 'Programming,Miscellaneous,Dark'
+#category_string = 'Programming,Miscellaneous,Dark'
+category_string = ''
 blacklist_string = 'nsfw,religious,political,racist,sexist'
+
+category.each do | key, value |
+  puts '#{key}: #{value}'
+  if value
+    category_string += key.to_s + ","
+  end
+end
+
+category_string.delete_suffix!(',')
+puts category_string
+
 uri = base_uri + category_string + '?blacklistFlags=' + blacklist_string
 
 #todo: dynamically generate this link based on true / false of above values
 #response = HTTParty.get('https://sv443.net/jokeapi/v2/joke/Programming,Miscellaneous,Dark?blacklistFlags=nsfw,religious,political,racist,sexist')
 response = HTTParty.get(uri)
-p response.parsed_response["category"]
+p response.parsed_response['category']
+p uri
